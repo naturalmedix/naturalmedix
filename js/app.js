a462e469f3b9b8c48c25a1b4158c84a320c9135c
naturalmedix <www.naturalmedix.co@gmail.com>
2026-07-31 01:11:10 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,8 +13,34 @@ const PRODUCTS = [
     image: "assets/images/natural-medix.mp4"
   },
   {
-    id: "aguaje-pawer",
-    name: "AGUAJE pawer",
+    id: "aguaje-plus",
+    name: "Aguaje Plus",
+    badge: "Estrella",
+    fabricado: "Natural Medix (Peru)",
+    netContent: "Cont. Neto: 100 Capsulas | Capsula 500mg.",
+    invima: "Producto sin registro",
+    benefit: "Regenera articulaciones, fortalece cabello, uñas y elasticidad de la piel.",
+    usage: "Tomar 3 capsulas diarias, una cada media hora antes de las comidas.",
+    price: 11000,
+    originalPrice: 22000,
+    image: "assets/images/natural-medix.mp4"
+  }
+  {
+    id: "aguaje-hinojo",
+    name: "Aguaje Hinojo",
+    badge: "Estrella",
+    fabricado: "Natural Medix (Peru)",
+    netContent: "Cont. Neto: 100 Capsulas | Capsula 500mg.",
+    invima: "Producto sin registro",
+    benefit: "Regenera articulaciones, fortalece cabello, uñas y elasticidad de la piel.",
+    usage: "Tomar 3 capsulas diarias, una cada media hora antes de las comidas.",
+    price: 11000,
+    originalPrice: 22000,
+    image: "assets/images/natural-medix.mp4"
+  }
+ {
+    id: "aguaje-siempre-bella",
+    name: "Aguaje Siempre Bella",
     badge: "Estrella",
     fabricado: "Natural Medix (Peru)",
     netContent: "Cont. Neto: 100 Capsulas | Capsula 500mg.",
