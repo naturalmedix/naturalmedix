aad3680169e1f25cd855a9a5e8934d389106b082
naturalmedix <www.naturalmedix.co@gmail.com>
2026-08-02 13:02:31 -0500

    Update manifest.json path in service worker

--- public/sw.js
+++ public/sw.js
@@ -9,7 +9,7 @@ const CACHE_NAME = "naturalmedix-v1.1.5";
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.json",
+  "./public/manifest.json",
   
   // Archivos CSS
   "./public/assets/css/styles.css",
