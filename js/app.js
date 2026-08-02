8f44679cf5498acdebfb199d6b9006bbed042627
naturalmedix <www.naturalmedix.co@gmail.com>
2026-08-02 00:37:11 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
    INICIALIZACIÓN DE LA APLICACIÓN (ENTRYPOINT)
    ========================================== */
 
-const APP_VERSION = "1.2.6";
+const APP_VERSION = "1.1.4";
 
 if (localStorage.getItem("app_version") !== APP_VERSION) {
   localStorage.setItem("app_version", APP_VERSION);
