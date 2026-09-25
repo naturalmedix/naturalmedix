ced6927ce946b997cdb1242c4d3218deaf8674f1
Natural Medix <www.naturalmedix.co@gmail.com>
2026-09-25 11:16:32 -0500

    Configure Astro for Cloudflare deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  site: 'https://naturalmedix.co',
+  output: 'server',
+  adapter: cloudflare(),
+  server: {
+    port: 4321,
+  },
 });
